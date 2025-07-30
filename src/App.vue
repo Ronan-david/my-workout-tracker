65ce699010d665e58ab9dc01dfe8b1a76f0417a9
Ronan David <ronan.david@consulting-for.accor.com>
2025-07-30 21:31:06 +0200

    Init SPA project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </nav>
+  <main>
+    <router-view />
+  </main>
+</template>
+
+<script setup lang="ts">
+// No script needed for this basic layout
+</script>
+
+<style scoped lang="scss">
+main {
+  text-align: center;
+}
+</style>
